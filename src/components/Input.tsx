5dfe031ad47426abc82041df50c9c06c21c4cf22
cbdyzj <cbdyzj@gmail.com>
2026-02-27 14:06:26 +0800

    Simplify Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,26 @@
-import { type InputHTMLAttributes } from 'react'
+import { type InputHTMLAttributes, type LegacyRef } from 'react'
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'ref'> {
   label: string
+  inputRef?: LegacyRef<HTMLInputElement>
 }
 
-export function Input({ label, ...props }: InputProps) {
+export function Input({ label, inputRef, ...props }: InputProps) {
   return (
     <div className="relative">
       <input
+        ref={inputRef}
+        id={props.id}
         {...props}
-        className="w-full p-[1.375rem_1.25rem_0.75rem] text-[0.9375rem] text-[#18181b] bg-[#fafafa] border border-[#e4e4e7] rounded-[14px] outline-none transition-all duration-200 focus:border-[#18181b] focus:bg-white focus:shadow-[0_0_0_4px_rgba(24,24,27,0.04)] placeholder-transparent"
-        placeholder=""
+        className="peer w-full px-5 py-[1.375rem] text-[0.9375rem] text-[#18181b] bg-[#fafafa] border border-[#e4e4e7] rounded-[14px] outline-none transition-colors focus:border-[#18181b] focus:bg-white"
+        placeholder=" "
       />
-      <label 
+      <label
         htmlFor={props.id}
-        className="absolute left-5 top-1/2 -translate-y-1/2 text-[0.9375rem] text-[#a1a1aa] pointer-events-none transition-all duration-200"
+        className="absolute left-5 top-1/2 -translate-y-1/2 text-[0.9375rem] text-[#a1a1aa] pointer-events-none transition-all peer-focus:top-[0.75rem] peer-focus:text-[0.6875rem] peer-focus:text-[#71717a] peer-not-placeholder-shown:top-[0.75rem] peer-not-placeholder-shown:text-[0.6875rem] peer-not-placeholder-shown:text-[#71717a]"
       >
         {label}
       </label>
-      <div className="absolute bottom-0 left-5 right-5 h-[1.5px] bg-[#18181b] scale-x-0 origin-center transition-transform duration-300 rounded-b-[14px]" />
     </div>
   )
 }
